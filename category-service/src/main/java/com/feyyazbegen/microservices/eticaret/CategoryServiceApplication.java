227a0e9cdf8ca661657ec192d745b638d0184741
Feyyaz Beğen <feyyazbegen61@gmail.com>
2023-01-20 22:39:47 +0300

    Projenin ismi category-service olarak değiştirildi

--- category-service/src/main/java/com/feyyazbegen/microservices/eticaret/CategoryServiceApplication.java
+++ category-service/src/main/java/com/feyyazbegen/microservices/eticaret/CategoryServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ETicaretApplication {
+public class CategoryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ETicaretApplication.class, args);
+		SpringApplication.run(CategoryServiceApplication.class, args);
 	}
 
 }
